1650c24ec2051fb61bfdf88b06d95c7b3a2d507d
Abbas Shamshi <abbasshamshi72@gmail.com>
2020-05-06 15:26:02 -0400

    footer

--- footer.php
+++ footer.php
@@ -8,7 +8,8 @@
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="footer-text">Copyright © <script>
                             document.write(new Date().getFullYear())
-                        </script> All rights reserved. Meet-Players | Developed by <a href="https://www.linkedin.com/in/abbas-shamshi/">Abbas Shmashi</div>
+                        </script> 
+                        All rights reserved. Meet-Players | Developed by <a href="https://www.linkedin.com/in/abbas-shamshi/">Abbas Shmashi</div>
                 </div>
             </div>
         </div>
