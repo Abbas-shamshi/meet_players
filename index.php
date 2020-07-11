919308b9ab4d485ec80be0429cfcd9181e307e78
Abbas Shamshi <abbasshamshi72@gmail.com>
2020-07-11 17:28:10 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/Abbas-shamshi/meet_players""
    This reverts commit e991dd29558bb51fd646b6307cc398a1c216ad05.

--- index.php
+++ index.php
@@ -3,94 +3,90 @@ require_once("config.php");
 require_once("functions.php");
 require_once("head.php");
 require_once("header.php");
-$profiles = fetchShortProfiles();
+if (!empty($_POST)) {
+    if (!empty($_POST['fname'])) {
+        $fname = "%" . $_POST['fname'] . "%";
+    } else {
+        $fname = "%%";
+    }
+    if (!empty($_POST['lname'])) {
+        $lname = "%" . $_POST['lname'] . "%";
+    } else {
+        $lname = "%%";
+    }
+    if (!empty($_POST['location'])) {
+        $location = "%" . $_POST['location'] . "%";
+    } else {
+        $location = "%%";
+    }
+    if (!empty($_POST['sport'])) {
+        $sport = "%" . $_POST['sport'] . "%";
+    } else {
+        $sport = "%%";
+    }
+
+    $profiles = fetchSearcProfiles($fname, $lname, $location, $sport);
+} else {
+    $profiles = fetchShortProfiles();
+}
 
 ?>
-<div class="row">
-    <div class="col-lg-2 col-md-2 col-sm-12 col-sm-12 margin-top searchBar">
-        <h3>Search</h3>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-12 col-sm-12 margin-top">
+                <h3>Search</h3>
 
-        <form id="search" action="searchfname.php" method="post">
-            <div class="profile-data">
-                <p>
-                    <label>first name:</label>
-                    <input type="text" name="fname" />
-                </p>
-                <p>
-                    <label>&nbsp;</label>
-                    <input type="submit" value="search" class="submit" />
-                </p>
-            </div>
-        </form>
-        <form id="search" action="searchlname.php" method="post">
-            <div class="profile-data">
-                <p>
-                    <label>Last name:</label>
-                    <input type="text" name="lname" />
-                </p>
-                <p>
-                    <label>&nbsp;</label>
-                    <input type="submit" value="search" class="submit" />
-                </p>
-            </div>
-        </form>
-        <form id="search" action="searchlocation.php" method="post">
-            <div class="profile-data">
-                <p>
-                    <label>Location:</label>
-                    <input type="text" name="loc" />
-                </p>
-                <p>
-                    <label>&nbsp;</label>
-                    <input type="submit" value="search" class="submit" />
-                </p>
-            </div>
-        </form>
-        <form id="search" action="searchsport.php" method="post">
-            <div class="profile-data">
-                <p>
-                    <label>Sport:</label>
-                    <input type="text" name="spo" />
-                </p>
-                <p>
-                    <label>&nbsp;</label>
-                    <input type="submit" value="search" class="submit" />
-                </p>
-            </div>
-        </form>
-    </div>
-    <div class="col-lg-9 col-md-9 col-sm-12 col-sm-12 margin-top">
-        <?php $i = 0;
-        foreach ($profiles as $profile) {
-            $i++;
-        ?>
+                <form id="search" action="<?php $_SERVER["PHP_SELF"]; ?>" method="post">
+                    <div class="profile-data">
+                        <p>
+                            <label>first name:</label>
+                            <input type="text" name="fname" />
+                        </p>
 
-            <div class=" container card ">
-                <a href="fullProfile.php?pro_id=<?php print $profile['id'] ?>">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 col-sm-12 col-sm-12 card-img">
 
-                            <img src=<?php print $profile['image'] ?> alt="Avatar" style="width:100%">
+                        <p>
+                            <label>Last name:</label>
+                            <input type="text" name="lname" />
+                        </p>
+
+
+                        <p>
+                            <label>Location:</label>
+                            <input type="text" name="location" />
+                        </p>
+
+                        <p>
+                            <label>Sport:</label>
+                            <input type="text" name="sport" />
+                        </p>
+                        <p>
+                            <button type="submit" class="btn btn-color" value="submit">Submit</button>
+                        </p>
+                    </div>
+                </form>
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-12 col-sm-12 margin-top">
+            <?php $i = 0;
+            foreach ($profiles as $profile) {
+                $i++;
+            ?>
+
+                <div class=" container card ">
+                    <a href="fullProfile.php?pro_id=<?php print $profile['id'] ?>">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4 col-sm-12 col-sm-12 card-img">
+
+                                <img src=<?php print $profile['image'] ?> alt="Avatar" style="width:100%">
 
 
 
-                        </div>
+                            </div>
 
-                        <div class="col-lg-8 col-md-8 col-sm-12 col-sm-12">
-
-
+                            <div class="col-lg-8 col-md-8 col-sm-12 col-sm-12">
 
                             <div class="p_container">
                                 <h4><b><?php print $profile['firstname'] . " ";
                                         print $profile['lastname'] ?></b></h4>
-                                <!-- <div class="flex">
-                            <div class="flex_child1">Sport: Football</div>
-                            <div class="flex_child2">position: Defender</div>
-                        </div>
-                        <div class="flex">
-                            <div class="flex_child1">College: Pace</div>
-                            <div class="flex_child2">team: Setters</div>
-                        </div> -->
                                 <div class="flex">
                                     <div class="">
                                         <p>Sport: <?php print $profile['sport'] ?></p>
@@ -100,33 +96,38 @@ $profiles = fetchShortProfiles();
                                         <!-- <p>Age: <?php print $profile['age'] ?></p> -->
 
 
+                                <div class="p_container">
+                                    <h4><b><?php print $profile['firstname'] . " ";
+                                            print $profile['lastname'] ?></b></h4>
+                                    <div class="flex">
+                                        <div class="">
+                                            <p>Sport: <?php print $profile['sport'] ?></p>
+                                            <!-- <p>College: <?php print $profile['college'] ?></p> -->
+                                            <p>Location: <?php print $profile['location'] ?></p>
+                                            <!-- <p>Experience: <?php print $profile['experience'] ?></p> -->
+                                            <!-- <p>Age: <?php print $profile['age'] ?></p> -->
+
+
+
+                                        </div>
+
+                                        <div class="flex_child2">
+                                            <p>position: <?php print $profile['position'] ?></p>
+                                            <p>Team: <?php print $profile['team'] ?></p>
+                                            <!-- <p>Gender: <?php print $profile['gender'] ?></p> -->
+                                            <!-- <p>HowOften you play: <?php print $profile['howoften'] ?></p> -->
+
+                                            <!-- <p>Height: <?php print $profile['height'] ?></p> -->
+                                        </div>
 
                                     </div>
-                                    <!-- <div class="flex_child2">
-                                <div> </div>
-                                <div></div>
-                                <p></p>
-
-                            </div> -->
-                                    <div class="flex_child2">
-                                        <p>position: <?php print $profile['position'] ?></p>
-                                        <p>Team: <?php print $profile['team'] ?></p>
-                                        <!-- <p>Gender: <?php print $profile['gender'] ?></p> -->
-                                        <!-- <p>HowOften you play: <?php print $profile['howoften'] ?></p> -->
-
-                                        <!-- <p>Height: <?php print $profile['height'] ?></p> -->
-
-
-
-                                    </div>
-
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-        <?php } ?>
+                    </a>
+                </div>
+            <?php } ?>
 
 
+        </div>
     </div>
